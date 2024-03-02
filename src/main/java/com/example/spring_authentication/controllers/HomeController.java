3ad2ae6594dce04690ba503603720fe8d87e24b6
lazaroalexandre <lazaroalexandre2022@gmail.com>
2024-03-02 19:50:46 -0300

    Removendo credenciais

--- src/main/java/com/example/spring_authentication/controllers/HomeController.java
+++ src/main/java/com/example/spring_authentication/controllers/HomeController.java
@@ -1,14 +1,40 @@
 package com.example.spring_authentication.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring_authentication.enums.Role;
+import com.example.spring_authentication.models.User;
+import com.example.spring_authentication.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "Bem-vindo!";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @SuppressWarnings("rawtypes")
+    @GetMapping("/home/{userId}")
+    public ResponseEntity home(@PathVariable String userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (user.getRole() == Role.USER) {
+                return ResponseEntity.status(HttpStatus.OK).body("Bem-vindo, " + user.getName() + "!"+
+                "\n" + "Você tem permissão de USER");
+            } else if (user.getRole() == Role.ADMIN) {
+                return ResponseEntity.status(HttpStatus.OK).body("Bem-vindo, " + user.getName() + "!" +
+                "\n" + "Você tem permissão de ADMIN");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Bem-vindo!\nVocê precisa se registrar ou fazer login para entrar no sistema.");
     }
 }
--- src/main/java/com/example/spring_authentication/dto/AuthenticateDto.java
+++ src/main/java/com/example/spring_authentication/dto/AuthenticateDto.java
@@ -1,4 +1,4 @@
 package com.example.spring_authentication.dto;
 
-public record AuthenticateDto(String email, String password) {
+public record AuthenticateDto(String email) {
 }
